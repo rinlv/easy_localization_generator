b8f74f299d73af4f04cfc8aea20bd4c518ad8680
svonidze <svonidze@mail.ru>
2022-12-06 10:26:10 +0300

    use the local localization file if no Google sheet id

--- lib/src/localization_generator.dart
+++ lib/src/localization_generator.dart
@@ -12,40 +12,75 @@ import 'csv_parser.dart';
 import 'sheet_localization.dart';
 
 class LocalizationGenerator extends GeneratorForAnnotation<SheetLocalization> {
-  @override
-  FutureOr<String> generateForAnnotatedElement(Element element, ConstantReader annotation, BuildStep buildStep) => _generateSource(element, annotation);
+  static const _urlFormat = 'https://docs.google.com/spreadsheets/export?format=csv&id=';
+  static const _headers = {
+    'Content-Type': 'text/csv; charset=utf-8',
+    'Accept': '*/*'
+  };
 
-  Future<String> _generateSource(Element element, ConstantReader annotation) async {
-    final headers = {'Content-Type': 'text/csv; charset=utf-8', 'Accept': '*/*'};
-    final response =
-        await http.get(Uri.parse('https://docs.google.com/spreadsheets/export?format=csv&id=${annotation.read('docId').stringValue}'), headers: headers);
+  @override
+  FutureOr<String> generateForAnnotatedElement(
+          Element element, ConstantReader annotation, BuildStep buildStep) =>
+      _generateSource(element, annotation);
+
+  Future<String> _generateSource(
+      Element element, ConstantReader annotation) async {
+    final docId = annotation.read('docId');
+    final outputDir = annotation.read('outDir').stringValue;
+    final outputFileName = annotation.read('outName').stringValue;
+    final preservedKeywords = annotation
+        .read('preservedKeywords')
+        .listValue
+        .map((e) => e.toStringValue())
+        .toList();
+    final current = Directory.current;
+    final output = Directory.fromUri(Uri.parse(outputDir));
+    final outputPath =
+        Directory(path.join(current.path, output.path, outputFileName));
+
     final classBuilder = StringBuffer();
-    classBuilder.writeln('// Generated at: ${formatDateWithOffset(DateTime.now().toLocal())}');
+    classBuilder.writeln(
+        '// Generated at: ${formatDateWithOffset(DateTime.now().toLocal())}');
     classBuilder.writeln('class ${element.displayName.substring(1)}{');
-    if (response.statusCode == 200) {
-      final outputDir = annotation.read('outDir').stringValue;
-      final outputFileName = annotation.read('outName').stringValue;
-      final preservedKeywords = annotation.read('preservedKeywords').listValue.map((e) => e.toStringValue()).toList();
-      final current = Directory.current;
-      final output = Directory.fromUri(Uri.parse(outputDir));
-      final outputPath = Directory(path.join(current.path, output.path, outputFileName));
+
+    void readCsv(File file) {
+      final data = file.readAsStringSync();
+      final csvParser = CSVParser(data);
+
+      classBuilder.writeln(csvParser.getSupportedLocales());
+      classBuilder
+          .writeln(csvParser.generateTranslationUsages(preservedKeywords));
+    }
+
+    if (docId.isNull) {
+      print("docId is null, going to use the file ${outputPath.path}");
+      final localFile = File(outputPath.path);
+      if (!localFile.existsSync()) {
+        throw Exception('File missing, path: ${outputPath.path}');
+      }
+
+      readCsv(localFile);
+    } else {
+      final response = await http.get(
+          Uri.parse(_urlFormat + docId.stringValue),
+          headers: _headers);
+      if (response.statusCode != 200) {
+        throw Exception('http reasonPhrase: ${response.reasonPhrase}');
+      }
 
       final generatedFile = File(outputPath.path);
       if (!generatedFile.existsSync()) {
         generatedFile.createSync(recursive: true);
       }
       generatedFile.writeAsBytesSync(response.bodyBytes);
-      final csvParser = CSVParser(response.body);
-      classBuilder.writeln(csvParser.getSupportedLocales());
-      classBuilder.writeln(csvParser.generateTranslationUsages(preservedKeywords));
-    } else {
-      throw Exception('http reasonPhrase: ${response.reasonPhrase}');
+      readCsv(generatedFile);
     }
     classBuilder.writeln('}');
     return classBuilder.toString();
   }
 
-  String formatDateWithOffset(DateTime date, {String format = 'EEE, dd MMM yyyy HH:mm:ss'}) {
+  String formatDateWithOffset(DateTime date,
+      {String format = 'EEE, dd MMM yyyy HH:mm:ss'}) {
     String twoDigits(int n) => n >= 10 ? "$n" : "0$n";
 
     final hours = twoDigits(date.timeZoneOffset.inHours.abs());
--- lib/src/sheet_localization.dart
+++ lib/src/sheet_localization.dart
@@ -1,15 +1,16 @@
 class SheetLocalization {
+  /// Optional. If null then the local file (outName) will be used
   final String? docId;
   final int version;
   final String outDir; //output directory
   final String outName; //output file name
   final List<String> preservedKeywords;
 
-  const SheetLocalization(
-      {this.docId,
-      this.version = 1,
-      this.outDir = 'resources/langs',
-      this.outName = 'langs.csv',
-      this.preservedKeywords = const []})
-      : assert(docId != null);
+  const SheetLocalization({
+    this.docId,
+    this.version = 1,
+    this.outDir = 'resources/langs',
+    this.outName = 'langs.csv',
+    this.preservedKeywords = const [],
+  });
 }
